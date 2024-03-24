45c865472b3682208d73d68f0a84edf477c59b4a
JoaoVitorRodriguesRocha <rochajoao123@hotmail.com>
2024-03-24 17:51:37 -0300

    Update integral.js

--- TDD/integral.js
+++ TDD/integral.js
@@ -1,20 +1,34 @@
 function calcularIntegralDefinida(funcao, a, b, n = 10000){
-    // Aqui vai ser calculado a integral definida
 
-    /*for(let i = 1; i < n; i++){
-    if(x === Math.pow(x,i)){
-        i = i + 1;
-        x = Math.pow(x,i)/i;
-    }
-    }*/
+
+    // Aqui é onde é calculado a integral definida, utilizando a regra do trapezio
+    // O resultado sempre será um valor aproximado
+
+    /* A ideia básica por trás da Regra do Trapézio é aproximar a curva da função 
+    por segmentos de reta entre os pontos de integração e, em seguida, 
+    calcular a área sob esses segmentos de reta. Essa é uma abordagem de aproximação, 
+    e quanto mais trapézios forem usados para dividir o intervalo de integração, 
+    mais precisa será a aproximação.
+    */
+
+    // nesse caso n é a quantidade de trapezios q eu vou usar
+    // a e b são os valores maiores e menores respectivamente
+
+    // aqui vai ser calculado o tamanho dos subintervalos
     let h = (a - b) / n;
+
+    // aqui vai calcular a média do ponto médio dos trapezios (primeiro e ultimo)
     let integral = (funcao(b) + funcao(a)) / 2;
 
+    // aqui é feito o somatorio de cada subintervalo
     for (let i = 1; i < n; i++) {
         integral += funcao(b + i * h);
     }
 
+    // aqui é o resultado
     return Math.abs(integral) * h;
+
+
 
 
 }
